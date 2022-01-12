a6118528e64e5f6da5a0f6c96871cb1f78694816
80338398 <liangbo@oppo.com>
2022-01-12 17:44:31 +0800

    sort comprator

--- Java/Demo/src/main/java/org/shithappens/libs/collections/Test.java
+++ Java/Demo/src/main/java/org/shithappens/libs/collections/Test.java
@@ -1,6 +1,9 @@
 package org.shithappens.libs.collections;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -11,5 +14,25 @@ public class Test {
     public static void main(String[] args) {
         List<String> emp = new ArrayList<>();
         emp.forEach(e -> log.info("e"));
+        List<Integer> emp2 = new ArrayList<>();
+        emp2.add(1);
+        emp2.add(2);
+
+        emp2.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        log.info( JSON.toJSONString(emp2));
+
+        emp2.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2- o1;
+            }
+        });
+        log.info( JSON.toJSONString(emp2));
+
     }
 }
